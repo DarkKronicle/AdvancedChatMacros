2a509b2be4815e327224ba0ae062a85e86a47b94
DarkKronicle <darkkronicle@gmail.com>
2022-01-24 19:54:06 -0700

    More powerful Konstruct functions

--- src/main/java/io/github/darkkronicle/advancedchatmacros/impl/MacroMatchProcessor.java
+++ src/main/java/io/github/darkkronicle/advancedchatmacros/impl/MacroMatchProcessor.java
@@ -2,19 +2,26 @@ package io.github.darkkronicle.advancedchatmacros.impl;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import fi.dy.masa.malilib.config.options.ConfigBoolean;
 import fi.dy.masa.malilib.config.options.ConfigInteger;
 import fi.dy.masa.malilib.config.options.ConfigString;
 import fi.dy.masa.malilib.gui.GuiConfigsBase;
 import io.github.darkkronicle.Konstruct.NodeException;
+import io.github.darkkronicle.Konstruct.functions.Variable;
 import io.github.darkkronicle.Konstruct.nodes.Node;
 import io.github.darkkronicle.Konstruct.parser.NodeProcessor;
+import io.github.darkkronicle.Konstruct.reader.builder.InputNodeBuilder;
 import io.github.darkkronicle.Konstruct.reader.builder.NodeBuilder;
+import io.github.darkkronicle.Konstruct.type.KonstructObject;
+import io.github.darkkronicle.Konstruct.type.ListObject;
 import io.github.darkkronicle.advancedchatcore.config.SaveableConfig;
 import io.github.darkkronicle.advancedchatcore.interfaces.IJsonApplier;
 import io.github.darkkronicle.advancedchatcore.interfaces.IMatchProcessor;
 import io.github.darkkronicle.advancedchatcore.interfaces.IScreenSupplier;
+import io.github.darkkronicle.advancedchatcore.konstruct.StringMatchObject;
 import io.github.darkkronicle.advancedchatcore.util.FluidText;
 import io.github.darkkronicle.advancedchatcore.util.SearchResult;
+import io.github.darkkronicle.advancedchatcore.util.StringMatch;
 import io.github.darkkronicle.advancedchatcore.util.SyncTaskQueue;
 import io.github.darkkronicle.advancedchatmacros.AdvancedChatMacros;
 import io.github.darkkronicle.advancedchatmacros.config.MacrosConfigStorage;
@@ -41,6 +48,9 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
     private final SaveableConfig<ConfigInteger> delay = SaveableConfig.fromConfig("delay",
             new ConfigInteger(translate("delay"), 0, 0, 6000, translate("info.delay")));
 
+    private final SaveableConfig<ConfigBoolean> parseAsKonstruct = SaveableConfig.fromConfig("parseAsKonstruct",
+            new ConfigBoolean(translate("parseaskonstruct"), false, translate("info.parseaskonstruct")));
+
     private String old = null;
 
     private Node node;
@@ -54,6 +64,11 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
         reloadNode();
         NodeProcessor processor = KonstructFilter.getInstance().getProcessor().copy();
         processor.addVariable("input", text.getString());
+        List<KonstructObject<?>> matches = new ArrayList<>();
+        for (StringMatch m : search.getMatches()) {
+            matches.add(new StringMatchObject(m));
+        }
+        processor.addVariable("matches", Variable.of(new ListObject(matches)));
         String message = processor.parse(node).getResult().getContent().getString();
         if (MacrosConfigStorage.General.PREVENT_MACRO_RECURSION.config.getBooleanValue() && search.getFinder() != null && search.getFinder().isMatch(message, search.getSearch())) {
             AdvancedChatMacros.LOGGER.log(Level.WARN, "Auto message stopped to prevent recursion!");
@@ -77,7 +92,11 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
             return;
         }
         try {
-            node = new NodeBuilder(command.config.getStringValue()).build();
+            if (parseAsKonstruct.config.getBooleanValue()) {
+                node = new NodeBuilder(command.config.getStringValue()).build();
+            } else {
+                node = new InputNodeBuilder(command.config.getStringValue()).build();
+            }
         } catch (NodeException e) {
             AdvancedChatMacros.LOGGER.log(Level.WARN, "Error setting up macro processor!", e);
             node = new NodeBuilder("").build();
@@ -90,6 +109,7 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
         JsonObject obj = new JsonObject();
         obj.add("command", command.config.getAsJsonElement());
         obj.add("delay", delay.config.getAsJsonElement());
+        obj.add("parseAsKonstruct", parseAsKonstruct.config.getAsJsonElement());
         return obj;
     }
 
@@ -104,6 +124,9 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
         }
         if (obj.has("delay")) {
             delay.config.setValueFromJsonElement(obj.get("delay"));
+        }
+        if (obj.has("parseAsKonstruct")) {
+            delay.config.setValueFromJsonElement(obj.get("parseAsKonstruct"));
         }
         reloadNode();
     }
@@ -124,7 +147,7 @@ public class MacroMatchProcessor implements IMatchProcessor, IScreenSupplier, IJ
 
         @Override
         public List<ConfigOptionWrapper> getConfigs() {
-            return ConfigOptionWrapper.createFor(Arrays.asList(processor.command.config, processor.delay.config));
+            return ConfigOptionWrapper.createFor(Arrays.asList(processor.command.config, processor.delay.config, processor.parseAsKonstruct.config));
         }
 
     }
